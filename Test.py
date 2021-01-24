58d943c992a720619524ca0d9f095618a31279c8
Chris <76802896+chrisgnorris@users.noreply.github.com>
2021-01-24 15:33:36 +1300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,17 @@
+
+#Good resources----------------------
+
+#http://www.randalolson.com/2014/06/28/how-to-make-beautiful-data-visualizations-in-python-with-matplotlib/
+#https://towardsdatascience.com/plotting-with-python-c2561b8c0f1f
+#https://python-graph-gallery.com/
+# https://docs.bokeh.org/en/latest/docs/gallery.html
+
+
+#Specific projects----------------------
+
+# Make this when we have easy access to Crop Rego plant data https://docs.bokeh.org/en/latest/docs/gallery/iris_splom.html
+# Make this for light https://docs.bokeh.org/en/latest/docs/gallery/unemployment.html
+#Measure labour hours by site, time - https://python-graph-gallery.com/342-animation-on-3d-plot/
+# Make a word cloud from comments - https://python-graph-gallery.com/wordcloud/
+# Map of NZ with bubble size to indicate issue - specified from 3 categories, p, l, Q https://python-graph-gallery.com/bubble-map/
+# Create graph using Bokeh example for light - https://mode.com/blog/python-data-visualization-libraries/    https://github.com/bokeh/bokeh/tree/master/examples/app/weather
